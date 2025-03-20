5ed72c215c4f6638dc07a392803d05ec227ec6fd
DevSalmanCodes <salmanahma9990@gmail.com>
2025-03-20 15:18:06 +0500

    add history model and history provider

---
+++ lib/models/qr_model.dart
@@ -0,0 +1,7 @@
+class QRModel {
+  final String value;
+  final String image;
+  final DateTime timestamp;
+
+  QRModel({required this.value, required this.image, required this.timestamp});
+}
---
+++ lib/providers/history_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:qr_code_scanner/models/qr_model.dart';
+
+class HistoryProvider extends ChangeNotifier {
+  List<QRModel> _history = [];
+  List<QRModel> get history => _history;
+  Box<QRModel>? _historyBox;
+  HistoryProvider() {
+    _init();
+  }
+  void _init() async {
+    _historyBox = await Hive.openBox('history');
+    _history = _historyBox!.values.toList().reversed.toList();
+    notifyListeners();
+  }
+
+  void addHistory(String value, String image) {
+    final timestamp = DateTime.now();
+    final newEntry = QRModel(value: value, image: image, timestamp: timestamp);
+    _historyBox!.add(newEntry);
+    _history.add(newEntry);
+    notifyListeners();
+  }
+
+  void deleteHistory(int index) {
+    _historyBox!.deleteAt(index);
+    _history.removeAt(index);
+    notifyListeners();
+  }
+}
