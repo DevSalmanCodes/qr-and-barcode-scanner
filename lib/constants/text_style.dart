9d434858308295ecbe023b83d8cfe8a51571cf13
DevSalmanCodes <salmanahma9990@gmail.com>
2025-03-22 15:02:55 +0500

    add text style utility for consistent text formatting

---
+++ lib/constants/text_style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+TextStyle textStyle(
+    {double? fontSize, FontWeight? fontWeight, Color? color = Colors.white}) {
+  return TextStyle(color: color, fontSize: fontSize, fontWeight: fontWeight);
+}
