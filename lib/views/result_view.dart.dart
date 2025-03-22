4d352e67a5710435324ee8c1f0fafbe346de03c3
DevSalmanCodes <salmanahma9990@gmail.com>
2025-03-22 15:02:38 +0500

    add copy to clipboard functionality and enhance result view layout

--- lib/views/result_view.dart.dart
+++ lib/views/result_view.dart.dart
@@ -1,33 +1,52 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:qr_code_scanner/constants/text_style.dart';
+import 'package:qr_code_scanner/utils/show_snackbar.dart';
 
 class ResultView extends StatelessWidget {
   final String result;
   final String path;
   const ResultView({super.key, required this.result, required this.path});
 
+  void _copyToClipboard(String text) {
+    Clipboard.setData(ClipboardData(text: text))
+        .then((_) => showSnackBar('Copied to clipboard'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Result'),
+      ),
       body: SafeArea(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(
-                result,
-                style: const TextStyle(
-                    fontWeight: FontWeight.bold, fontSize: 18.0),
-              ),
+              Text(result,
+                  style:
+                      textStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
               const SizedBox(
                 height: 15.0,
               ),
               Image.file(
                 File(path),
                 height: 180.0,
-              )
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              IconButton(
+                  onPressed: () => _copyToClipboard(result),
+                  icon: const Icon(
+                    Icons.copy,
+                    color: Colors.blue,
+                    size: 40.0,
+                  ))
             ],
           ),
         ),
